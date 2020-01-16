2b7f6449528a39f5c8642631c28d5f625afe1a51
Trà Tấn Hiệu <tratanhieu97@gmail.com>
2020-01-16 18:09:42 +0700

    update upload image

--- src/components/atoms/ImageUpload/index.js
+++ src/components/atoms/ImageUpload/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from '@material-ui/core'
 import Input from "../Input"
+import { CloudUploadOutlined } from "@material-ui/icons";
 
 const ImageUpload = ({
     source = "",
@@ -23,18 +24,38 @@ const ImageUpload = ({
     return (
         <div>
             <div>
-                <Card>
-                    <img src={source} />
-                </Card>
+                <img style={{
+                    width: '300px',
+                    border: 'solid 1px #dee0e2',
+                    padding: '5px',
+                    boxShadow: '1px 1px 3px 1px #dee0e2',
+                    backgrounColor: '#fff'
+                }} src={source} />
             </div>
-            <div
-                style={{ width, height, lineHeight: `calc(${height} - 14px)` }}
-            >
-                <Input
-                    type="file"
-                    multiple
-                    onChange={onAdd}
-                />
+            <div>
+                <label style={{
+                    width: '295px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    cursor: 'pointer',
+                    padding: '6px 16px',
+                    fontSize: '0.875rem',
+                    minWidth: '64px',
+                    boxSizing: 'border-box',
+                    transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+                    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+                    fontWeight: '500',
+                    lineHeight: 1.75,
+                    borderRadius: '4px',
+                    letterSpacing: '0.02857em',
+                    textTransform: 'uppercase',
+                    color: '#fff',
+                    backgroundColor: '#3f51b5',
+                    boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)'
+                }} for="file-upload" class="custom-file-upload">
+                    <CloudUploadOutlined /> <span style={{ alignSelf: 'center', marginLeft: '8px' }}>Upload</span>
+                </label>
+                <input style={{ display: 'none' }} id="file-upload" type="file" onChange={onAdd} />
             </div>
         </div>
     )
