066d8e58d784476335d677aa3d62a8972aa504b0
Minh Phuong <44746692+kingphon@users.noreply.github.com>
2020-02-17 20:20:05 +0700

    done screen

--- src/components/pages/ProductTypeGroup/index.js
+++ src/components/pages/ProductTypeGroup/index.js
@@ -1,27 +1,42 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 
 import ProductTypeGroupModal from '../../organisms/ProductTypeGroup/ProductTypeGroupModal';
 import ProductTypeGroupTable from '../../organisms/ProductTypeGroup/ProductTypeGroupTable';
-import ProductTypeGroupFilter from '../../organisms/ProductTypeGroup/ProductTypeGroupFilter';
-import ProductTypeGroupHeader from '../../organisms/ProductTypeGroup/ProductTypeGroupHeader';
 import { resetSystemErrors } from '../../../redux/reducers/rootReducer';
+import ContentHeader from '../../organisms/ContentHeader';
+import { fetchAll, getCreateAction } from '../../../redux/reducers/productTypeGroupReducer';
+
+const Render = ({ createButtonLoading, onOpenCreate }) => (
+    <>
+        <ContentHeader
+            title="Product TypeGroup"
+            createButtonLoading={createButtonLoading} 
+            onOpenCreate={onOpenCreate}
+        />
+        <ProductTypeGroupTable />
+        <ProductTypeGroupModal />
+    </>
+)
 
 const ProductTypeGroup = () => {
+    const selector = useSelector(({
+        productTypeGroupReducer: { createButtonLoading } 
+    }) => ({ createButtonLoading }), shallowEqual)
+
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(resetSystemErrors())
+        dispatch(fetchAll())
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    return (
-        <>
-            <ProductTypeGroupHeader/>
-            <ProductTypeGroupFilter />
-            <ProductTypeGroupTable />
-            <ProductTypeGroupModal />
-        </>
-    )
+    const renderProps = {
+        ...selector,
+        onOpenCreate: () => dispatch(getCreateAction())
+    }
+
+    return <Render {...renderProps} />
 }
 export default ProductTypeGroup
